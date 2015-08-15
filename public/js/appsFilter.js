d1c069a32c5f58e7787dcf2d7bcb10034302872a
Audrea M <malialin.audrea@gmail.com>
2015-08-15 15:34:18 +0200

    Apps (filter)  order

--- public/js/appsFilter.js
+++ public/js/appsFilter.js
@@ -10,21 +10,49 @@ var MAX_FILTERED_APP_SIZE = 4;
 $.get("/all_apps_list", function (data) {
     $("#appsFilter").html('');
     var apps = data.apps;
+    var apps_counter = [];
     $.each(apps, function (i, item) {
-        $("#appsFilter").append("<li><input type='checkbox' value='" + item.name + "' class='filter app' id='" + item.name + "'><label for='" + item.name + "'>" + (item.name.length > 30 ? (item.name.substr(0, 27) + "...") : item.name) + "</label></li>");
+        var app_data = {};
+
 
         //Save apps list in LocalStorage
-        localStorage.setItem('app_' + item.id, item.name);
+        var app = localStorage.getItem('app_' + item.id);
+
+        if (app == null) {
+            app_data = {id: item.id, name: item.name, count: 0};
+            localStorage.setItem('app_' + item.id, JSON.stringify(app_data));
+        }
+        else {
+            app_data = JSON.parse(app);
+        }
+
+
+        apps_counter.push(app_data);
+
     });
+
+    printAppsByFilterCounter(apps_counter);
 
     $(".filter.app").change(function (e) {
         var appName = $(e.target).attr("value");
+        var appID = $(e.target).attr("id");
         if($(e.target).is(':checked')) {
             if(FiltredApps.length >= (MAX_FILTERED_APP_SIZE)){
                 Materialize.toast("You can not add another apps filter : the limit is " + MAX_FILTERED_APP_SIZE, 4000);
                 $(e.target).prop('checked', false);
                 return;
             }
+
+            //Save check counter in localStorage
+            var app = localStorage.getItem('app_' + appID);
+
+            app = JSON.parse(app);
+            var app_data = {id: app.id, name: app.name, count: app.count + 1};
+
+            localStorage.setItem('app_' + appID, JSON.stringify(app_data));
+
+
+
             var c = addLegend(appName);
             FiltredApps.push({name: appName, color: c});
             notifyAppsFilterChanged();
@@ -38,3 +66,17 @@ $.get("/all_apps_list", function (data) {
     });
 });
 
+function printAppsByFilterCounter(apps) {
+    console.log(apps);
+    apps.sort(function (a, b) {
+        console.log(a.count - b.count);
+        return b.count - a.count;
+    });
+
+    console.log(apps);
+    for (var i = 0; i < apps.length; i++) {
+        console.log(apps[i].count);
+        $("#appsFilter").append("<li><input type='checkbox' value='" + apps[i].name + "' class='filter app' id='" + apps[i].id + "'><label for='" + apps[i].id + "'>" + (apps[i].name.length > 30 ? (apps[i].name.substr(0, 27) + "...") : apps[i].name) + "</label></li>");
+
+    }
+}
